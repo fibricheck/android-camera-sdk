ef2f392377f447c3b61b5c58fee44a6621cac355
Christopher Hex <che@christopherh.dev>
2023-04-13 10:27:08 +0200

    First working Github packages version

--- app/src/main/java/com/qompium/fibricheckexample/FirstFragment.java
+++ app/src/main/java/com/qompium/fibricheckexample/FirstFragment.java
@@ -9,8 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.qompium.fibricheck_camera_sdk
-
 import com.qompium.fibricheckexample.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
